ac5c58eea61ba78c217b170e42ddd0ff21cbc449
Kristine Mauric <kristine.mauric@gmail.com>
2016-04-10 15:31:17 -0400

    Fixed navigation on Firefox and added animations on scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
+$(function() {
+  $(window).scroll(function() {
+    var scrollPos = $(document).scrollTop();
+    console.log(scrollPos);
 
+    if (scrollPos = 120) {
+      $("#services h2").addClass("animated flash");
+      $(".services-features").addClass("animated fadeInUp");
+    } else if (scrollPos = 790) {
+      $("#team h2").addClass("animated fadeInUp");
+    } else if (scrollPos = 1100) {
+      $(".team-member").addClass("animated zoomIn");
+    } else {
+      scrollPos = 0;
+    }
+  });
+
+
+});
