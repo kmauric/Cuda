c93ac4cbf25ebb79e57a69d69d581b2b53346a74
Kristine Mauric <kristine.mauric@gmail.com>
2016-04-12 12:48:50 -0400

    added wow.js in js folder and added animations on scroll in index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,21 @@
 $(function() {
-  $(window).scroll(function() {
-    var scrollPos = $(document).scrollTop();
-    console.log(scrollPos);
-
-    if (scrollPos = 120) {
-      $("#services h2").addClass("animated flash");
-      $(".services-features").addClass("animated fadeInUp");
-    } else if (scrollPos = 790) {
-      $("#team h2").addClass("animated fadeInUp");
-    } else if (scrollPos = 1100) {
-      $(".team-member").addClass("animated zoomIn");
-    } else {
-      scrollPos = 0;
-    }
-  });
+	
+	new WOW().init();
+//  $(window).scroll(function() {
+//    var scrollPos = $(document).scrollTop();
+//    console.log(scrollPos);
+//
+//    if (scrollPos = 120) {
+//      $("#services h2").addClass("animated flash");
+//      $(".services-features").addClass("animated fadeInUp");
+//    } else if (scrollPos = 790) {
+//      $("#team h2").addClass("animated fadeInUp");
+//    } else if (scrollPos = 1100) {
+//      $(".team-member").addClass("animated zoomIn");
+//    } else {
+//      scrollPos = 0;
+//    }
+//  });
 
 
 });
