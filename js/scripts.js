6f5832f597c95e54e58b01ac1d9bf2c057787afe
Kristine Mauric <kristine.mauric@gmail.com>
2016-04-14 14:02:54 -0400

    Added radial progress bar jQuery plugin and removed typed.min.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,53 @@
 $(function() {
 	
 	new WOW().init();
-//  $(window).scroll(function() {
-//    var scrollPos = $(document).scrollTop();
-//    console.log(scrollPos);
-//
-//    if (scrollPos = 120) {
-//      $("#services h2").addClass("animated flash");
-//      $(".services-features").addClass("animated fadeInUp");
-//    } else if (scrollPos = 790) {
-//      $("#team h2").addClass("animated fadeInUp");
-//    } else if (scrollPos = 1100) {
-//      $(".team-member").addClass("animated zoomIn");
-//    } else {
-//      scrollPos = 0;
-//    }
-//  });
+	
+	$("#web-design-circle").circleProgress({
+		value: 0.9,
+		size: 160,
+		startAngle: -Math.PI / 2,
+		fill: {
+			color: "#30bae7"
+		}
+	}).on('circle-animation-progress', function(event, progress) {
+    $(this).find('.percentile-radius').html(parseInt(90 * progress) + '<i>%</i>');
+		
+	}); //end #web-design-circle progress
+	
+	$("#html-css-circle").circleProgress({
+		value: 0.75,
+		size: 160,
+		startAngle: -Math.PI / 2,
+		fill: {
+			color: "#d74680"
+		}
+	}).on('circle-animation-progress', function(event, progress) {
+    $(this).find('.percentile-radius').html(parseInt(75 * progress) + '<i>%</i>');
+		
+	}); //end #html-css-circle progress
+	
+	$("#graphic-design-circle").circleProgress({
+		value: 0.7,
+		size: 160,
+		startAngle: -Math.PI / 2,
+		fill: {
+			color: "#15c7a8"
+		}
+	}).on('circle-animation-progress', function(event, progress) {
+    $(this).find('.percentile-radius').html(parseInt(70 * progress) + '<i>%</i>');
+		
+	}); //end #graphic-design-circle progress
+	
+	$("#ux-circle").circleProgress({
+		value: 0.85,
+		size: 160,
+		startAngle: -Math.PI / 2,
+		fill: {
+			color: "#eb7d4b"
+		}
+	}).on('circle-animation-progress', function(event, progress) {
+    $(this).find('.percentile-radius').html(parseInt(85 * progress) + '<i>%</i>');
+		
+	}); //end ux-circle progress
 
-
-});
+}); //end jQuery
